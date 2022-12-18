6f5100d230cd6303a7e1adb76075cc82d1e23087
aletsmc07 <alejandro.mince07@gmail.com>
2022-12-18 01:04:26 -0600

    Portfolio created
    Require update images, url, title, description for each card

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -1,10 +1,53 @@
 import React from "react";
+import {Data} from './dataPortfolio';
+import  githubIcon from '../../Assets/images/footer images/github-ico.png';
+import './portfolio.css';
+
+const styles = {
+    section: {
+        minHeight: '85vh',
+    },
+    card: {
+        width: '350px',
+        height: '480px',
+    },
+    img: {
+        maxHeight: '255px',
+        fit: 'cover',
+    },
+    linkButton: {
+        position: 'absolute',
+        bottom: '20px',
+    },
+    githubButton: {
+        position: 'absolute',
+        height: '41px',
+        bottom: '20px',
+        right: '20px',
+    }
+};
 
 const Portfolio = () => {
     return (
-        <section>
-            <div className="portfolio">Portfolio</div>
-        </section>
+        <div style={styles.section}>
+            <ul className="d-flex flex-wrap align-items-center justify-content-center p-0 m-5">
+                {Data.map(project => (
+                    <div class="card bg-dark text-white Cardstyle">
+                    <img src={project.image} className="card-img-top" alt={project.title} />
+                    <div class="card-body">
+                      <h5 class="card-title text-center">{project.title}</h5>
+                      <p class="card-text text-center">{project.desc}</p>
+                      <a href={project.url} class="btn btn-dark">
+                        <i class="bi bi-browser-chrome "></i>
+                      </a>
+                      <a href={project.GithubRep} class="btn btn-dark">
+                        <img src={githubIcon} alt="...GithubLink" className="btn btn-dark" style={styles.githubButton} />
+                      </a>
+                    </div>
+                  </div>
+                ))}
+            </ul>
+        </div>
     )
 }
 
---
+++ src/Components/Portfolio/dataPortfolio.js
@@ -0,0 +1,35 @@
+export const Data = [
+    {
+        id: 1,
+        title: "PROJECT TITLE",
+        desc: "Some description text",
+        image: "https://i0.wp.com/asphalt9.info/wp-content/uploads/2022/11/Koenigsegg-Agera-RS.jpg?fit=1062%2C588&ssl=1",
+        url: "https://aletsmc07.github.io/mywebsite.github.io/",
+        GithubRep: "https://github.com/aletsmc07/mywebsite.github.io",
+    },
+    {
+        id: 2,
+        title: "PROJECT TITLE",
+        desc: "Some description text",
+        image: "https://i0.wp.com/asphalt9.info/wp-content/uploads/2022/11/Koenigsegg-Agera-RS.jpg?fit=1062%2C588&ssl=1",
+        url: "https://aletsmc07.github.io/mywebsite.github.io/",
+        GithubRep: "https://github.com/aletsmc07/mywebsite.github.io",
+    },
+    {
+        id: 3,
+        title: "PROJECT TITLE",
+        desc: "Some description text",
+        image: "https://i0.wp.com/asphalt9.info/wp-content/uploads/2022/11/Koenigsegg-Agera-RS.jpg?fit=1062%2C588&ssl=1",
+        url: "https://aletsmc07.github.io/mywebsite.github.io/",
+        GithubRep: "https://github.com/aletsmc07/mywebsite.github.io",
+    },
+    {
+        id: 4,
+        title: "PROJECT TITLE",
+        desc: "Some description text",
+        image: "https://i0.wp.com/asphalt9.info/wp-content/uploads/2022/11/Koenigsegg-Agera-RS.jpg?fit=1062%2C588&ssl=1",
+        url: "https://aletsmc07.github.io/mywebsite.github.io/",
+        GithubRep: "https://github.com/aletsmc07/mywebsite.github.io",
+    },
+]
+
