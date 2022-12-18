25cc5821c59c0b77a0ae78cc1ef71d8a71ce438f
aletsmc07 <alejandro.mince07@gmail.com>
2022-12-17 23:37:37 -0600

    Update
    Created Componentes
    App,js updated
    Navbar updated

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,29 +1,29 @@
 import React from "react";
 import './header.css'
 
-const Header = () => {
+const Header = ({currentPage, handlePage}) => {
     return (
         <header className="header">
             <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
                 <div class="container-fluid">
-                    <a class="navbar-brand" href="https://github.com/aletsmc07/React-Portfolio">
-                        <img src="https://upload.wikimedia.org/wikipedia/commons/7/72/Logo_IPN.png" alt="Logo" width="30" height="24" class="d-inline-block align-text-top" />My Portfolio
+                    <a class="navbar-brand" href="#About" onClick={() => handlePage('About')}>
+                        <img src="https://upload.wikimedia.org/wikipedia/commons/7/72/Logo_IPN.png" alt="Logo" width="30" height="24" class="d-inline-block align-text-top" />Eduardo Alejandro Mince Cruz
                     </a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
                     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                         <div class="navbar-nav ms-auto">
-                            <a class="nav-link" href="#about">
-                            <i class="bi bi-house"></i> About
+                            <a className= {currentPage === 'About' ? 'nav-link active' : 'nav-link'} href="#About" onClick={() => handlePage('About')}>
+                                <i class="bi bi-house"></i> About
                             </a>
-                            <a class="nav-link" href="#portfolio">
+                            <a className= {currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'} href="#Portfolio" onClick={() => handlePage('Portfolio')}>
                                 <i class="bi bi-file-earmark-code"></i>Portfolio
                             </a>
-                            <a class="nav-link" href="#contact">
+                            <a className= {currentPage === 'Contact' ? 'nav-link active' : 'nav-link'} href="#contact" onClick={ () => handlePage('Contact')}>
                                 <i class="bi bi-envelope"></i>Contact
                             </a>
-                            <a class="nav-link" href="#resume">
+                            <a className= {currentPage === 'Resume' ? 'nav-link active' : 'nav-link'} href="#resume" onClick={ () => handlePage('Resume')}>
                                 <i class="bi bi-file-person"></i>Resume
                             </a>
                         </div>
---
+++ src/Components/Portfolio/Portfolio.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Portfolio = () => {
+    return (
+        <section>
+            <div className="portfolio">Portfolio</div>
+        </section>
+    )
+}
+
+export default Portfolio
