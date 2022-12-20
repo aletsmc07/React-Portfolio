efd3c989efce8b87ba792d4d3882f25304acde3c
aletsmc07 <alejandro.mince07@gmail.com>
2022-12-19 19:08:04 -0600

    Final Touches

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import linkedinIcon from '../../Assets/images/footer images/linkedin-ico.png';
 import githubIcon from '../../Assets/images/footer images/github-ico.png';
 import instagramIcon from '../../Assets/images/footer images/insta-ico.png';
+import facebookIcon from '../../Assets/images/footer images/facebook-ico.png';
 
 const Footer = () => {
     const styles = {
@@ -13,13 +14,14 @@ const Footer = () => {
             display: 'flex',
             position: 'relative',
             justifyContent: 'center',
-            padding: '1.5rem',
+            padding: '0.5rem',
             width: '100%',
             bottom: '0vh'
         }
     }
     return (
-        <footer style={styles.footer} >
+        <footer className="d-flex flex-column align-items-center text-center text-light" style={styles.footer} >
+            <div>
             <a target="_blank" rel="noreferrer" href="https://github.com/aletsmc07">
                 <img
                     style={styles.icon}
@@ -41,6 +43,18 @@ const Footer = () => {
                     alt="icon"
                 />
             </a>
+            <a target="_blank" rel="noreferrer" href="https://www.facebook.com/aletsmc">
+                <img
+                    style={styles.icon}
+                    src={facebookIcon}
+                    alt="icon"
+                />
+            </a>
+            </div>
+            <div className="d-flex flex-column align-items-center text-center text-light">
+                <h6>&#169; All rights reserved.</h6>
+                <h7 className="mt-2">&#128076; Page made by Alejandro Mince, created in React. &#9996;</h7>
+            </div>
         </footer>
     )
 }
